eafb700a49a518c023ccef4537ab2fd180c4d1fb
santimc <santimonserr@hotmail.com>
2020-01-19 22:29:40 -0300

    Fixed bugs

--- script.js
+++ script.js
@@ -6,8 +6,11 @@ buttons.forEach((button) => {
 });
 
 function operate(operator, n1, n2){
-  n1 = parseInt(n1);
-  n2 = parseInt(n2);
+  if (/\D/.test(n2)){
+    return Number(n1);
+  }
+  n1 = Number(n1);
+  n2 = Number(n2);
   switch (operator) {
     case "+":
       return add(n1, n2);
@@ -22,9 +25,10 @@ function operate(operator, n1, n2){
 }
 
 function resolve(expression) {
-  let expressionArray = expression.split(/(\D)/).filter(Boolean);
+  let expressionArray = expression.split(/([^0-9^\.])/).filter(Boolean);
+  console.log(expressionArray);
   let arrayInitialLength = expressionArray.length
-  for (var i = 1; i < arrayInitialLength; i++) {
+  for (var j = 1; j < arrayInitialLength; j++) {
     expressionArray.forEach((item, i) => {
       if (item == "x" || item == "/") {
         expressionArray.splice(i - 1, 3 ,operate(expressionArray[i], expressionArray[i-1], expressionArray[i+1]));
@@ -34,6 +38,8 @@ function resolve(expression) {
       }
     });
   }
+  expressionArray[0] = Number(expressionArray[0]);
+  return +expressionArray[0].toFixed(4);
 }
 
 const add = (n1, n2) => n1 + n2;
@@ -44,8 +50,8 @@ const divide = (n1, n2) => n1 / n2;
 
 function display(e){
   //e.target.textContent is the value of the button
-  displayContent.value += e.target.textContent
+  displayContent.textContent += e.target.textContent
   if (e.target.textContent == "=") {
-    operate(displayContent.value);
+    displayContent.textContent = resolve(displayContent.textContent);
   }
 }
